b4ece234533319a395ac0d1aedc6a6a9c8375c01
Antoine GRAND <74561195+rtas-vadum@users.noreply.github.com>
2023-10-10 16:52:48 +0200

    MàJ pour MySQL 8

--- provision-db.sh
+++ provision-db.sh
@@ -11,6 +11,8 @@ mysql < data_sql_create.sql
 
 mysql -u root -p -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY '4#j4TA-qNf5yj5M5^3D+';"
 mysql -u root -p -e "GRANT ALL PRIVILEGES ON MonSite.* TO 'myuser'@'localhost';"
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON MonSite.* TO 'myuser'@'192.168.56.2';"
+mysql -u root -p -e "CREATE USER 'myuser'@'192.168.56.2' IDENTIFIED BY '4#j4TA-qNf5yj5M5^3D+';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON MonSite.* TO 'myuser'@'192.168.56.2' WITH GRANT OPTION;"
+
 
 . scriptDB.sh
